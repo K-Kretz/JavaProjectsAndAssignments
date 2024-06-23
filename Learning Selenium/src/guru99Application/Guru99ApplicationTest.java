6e23a3fb7cd92d6b3f4170d82545b8a1fb143788
keith <keith@DESKTOP-A68GSGO>
2024-06-23 18:34:49 -0400

    Finished Project

--- Learning Selenium/src/guru99Application/Guru99ApplicationTest.java
+++ Learning Selenium/src/guru99Application/Guru99ApplicationTest.java
@@ -14,6 +14,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class Guru99ApplicationTest {
@@ -22,7 +23,7 @@ public class Guru99ApplicationTest {
 	
 	private String url = "http://demo.guru99.com/v4";
 	
-	@BeforeMethod
+	@BeforeTest
 	public void invokeBrowser() {
 		
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\keith\\Desktop\\Selenium Testing\\chromedriver.exe");
@@ -58,5 +59,46 @@ public class Guru99ApplicationTest {
 		userPassword.sendKeys("EjYpery");
 		
 		loginButton.click();
+	}
+	
+	@Test(priority=2)
+	public void addCustomer() {
+		
+		WebElement addCustomerLink = driver.findElement(By.linkText("New Customer"));
+		
+		addCustomerLink.click();
+		
+		driver.findElement(By.name("name")).sendKeys("Keith Barn");
+		
+		driver.findElement(By.xpath("//input[@value='f']")).click();
+		
+		driver.findElement(By.name("dob")).sendKeys("05/05/1995");
+		
+		driver.findElement(By.name("addr")).sendKeys("30566 NewYork dr");
+		
+		driver.findElement(By.name("pinno")).sendKeys("555555");
+		
+		driver.findElement(By.name("state")).sendKeys("California");
+		
+		driver.findElement(By.name("city")).sendKeys("Las Vegas");
+		
+		driver.findElement(By.name("telephoneno")).sendKeys("45444445555");
+		
+		driver.findElement(By.name("emailid")).sendKeys("abc1er425@xyze.com");
+		
+		driver.findElement(By.name("password")).sendKeys("Poiy@123");
+		
+		driver.findElement(By.name("sub")).click();
+		
+		
+		
+	}
+	
+	@Test(priority=3)
+	public void getCustomerId() {
+		
+		String customerId = driver.findElement(By.xpath("//table[@id='customer']/tbody/tr[4]/td[2]")).getText();
+		
+		System.out.println("ID: " + customerId);
 	}
 }
