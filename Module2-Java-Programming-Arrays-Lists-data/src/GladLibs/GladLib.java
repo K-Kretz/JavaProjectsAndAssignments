0b540b618edd60ae85d904c99cf450c0fa464e14
keith <keith@DESKTOP-A68GSGO>
2024-07-08 17:43:47 -0400

    -modify program:
    	-once it uses a word, it never uses that word again.
    
    	-total number of words that were replaced right after the story is
    	printed.

--- Module2-Java-Programming-Arrays-Lists-data/src/GladLibs/GladLib.java
+++ Module2-Java-Programming-Arrays-Lists-data/src/GladLibs/GladLib.java
@@ -15,6 +15,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Random;
 
 public class GladLib {
@@ -28,6 +29,9 @@ public class GladLib {
 	private ArrayList<String> nameList;
 	private ArrayList<String> animalList;
 	private ArrayList<String> timeList;
+	private ArrayList<String> verbList;
+	private ArrayList<String> fruitList;
+	private ArrayList<String> usedWords;
 	
 	private Random myRandom;
 	
@@ -51,7 +55,10 @@ public class GladLib {
 		countryList = readIt(source+"\\country.txt");
 		nameList = readIt(source+"\\name.txt");		
 		animalList = readIt(source+"\\animal.txt");
-		timeList = readIt(source+"\\timeframe.txt");		
+		timeList = readIt(source+"\\timeframe.txt");	
+		verbList = readIt(source+"\\verb.txt");
+		fruitList = readIt(source+"\\fruit.txt");
+		usedWords = new ArrayList<String>();
 	}
 	
 	private String randomFrom(ArrayList<String> source){
@@ -81,6 +88,12 @@ public class GladLib {
 		if (label.equals("timeframe")){
 			return randomFrom(timeList);
 		}
+		if (label.equals("verb")){
+			return randomFrom(verbList);
+		}
+		if (label.equals("fruit")){
+			return randomFrom(fruitList);
+		}
 		if (label.equals("number")){
 			return ""+myRandom.nextInt(50)+5;
 		}
@@ -96,6 +109,11 @@ public class GladLib {
 		String prefix = w.substring(0,first);
 		String suffix = w.substring(last+1);
 		String sub = getSubstitute(w.substring(first+1,last));
+		while(usedWords.contains(sub))
+		{
+			sub = getSubstitute(w.substring(first+1,last));
+		}
+		usedWords.add(sub);
 		return prefix+sub+suffix;
 	}
 	
@@ -154,7 +172,9 @@ public class GladLib {
 	}
 	
 	private ArrayList<String> readIt(String source) throws IOException{
+		
 		ArrayList<String> list = new ArrayList<String>();
+		
 		if (source.startsWith("http")) 
 		{
 			URL resource = new URL(source);
@@ -184,8 +204,15 @@ public class GladLib {
 	
 	public void makeStory() throws IOException{
 	    System.out.println("\n");
-		String story = fromTemplate("C:\\Users\\keith\\git\\repository\\Module2-Java-Programming-Arrays-Lists-data\\src\\GladLibs\\data\\madtemplate.txt");
+		String story = fromTemplate("C:\\Users\\keith\\git\\repository\\Module2-Java-Programming-Arrays-Lists-data\\src\\GladLibs\\data\\madtemplate2.txt");
 		printOut(story, 60);
+		
+		System.out.println("\n");
+		for (int i=0; i<usedWords.size(); i++) {
+			System.out.println(usedWords.get(i));
+		}
+		
+		System.out.println(usedWords.size());
 	}
 
 
