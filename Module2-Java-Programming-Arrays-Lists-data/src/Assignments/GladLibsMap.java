532dd69c5ee9d2f03e3473bf2657352ffa165811
keith <keith@DESKTOP-A68GSGO>
2024-07-22 17:44:55 -0400

    updated comments

--- Module2-Java-Programming-Arrays-Lists-data/src/Assignments/GladLibsMap.java
+++ Module2-Java-Programming-Arrays-Lists-data/src/Assignments/GladLibsMap.java
@@ -1,5 +1,12 @@
 /**
+ * Assignment 3: GladLibsMap
  * 
+ * modify previous program to use one HashMap that maps word types to ArrayList of possible words to select. 
+ * Your program should still work for the additional categories verbs and fruits and should not use duplicate words from a category.
+ * 
+ * Developer: Keith Kretz
+ * Date Created: 7/22/2024
+ * Last Updated: 7/22/2024 
  */
 package Assignments;
 
