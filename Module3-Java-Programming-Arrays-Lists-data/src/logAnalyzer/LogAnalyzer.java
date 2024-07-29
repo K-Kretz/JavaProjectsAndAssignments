754dab9c8142a7e0dc613aceb3282a16e2af7948
keith <keith@DESKTOP-A68GSGO>
2024-07-29 17:16:57 -0400

    Additions: -add Test Class LogAnalyzerTest.java
    Modifications:
    -Methods readFile(String), printAll()
    
    Main:
    -create LogAnalyzer object to read a file and store in ArrayList then
    print all entries

---
+++ Module3-Java-Programming-Arrays-Lists-data/src/logAnalyzer/LogAnalyzer.java
@@ -0,0 +1,46 @@
+/**
+ * Class to Analyze the Log Entries
+ * 
+ * Developer: Keith Kretz
+ * Date Created: 7/29/2024
+ * Last Updated: 7/29/2024  
+ */
+package logAnalyzer;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+
+public class LogAnalyzer {
+	
+	private ArrayList<LogEntry> records;
+	
+	public LogAnalyzer() {
+		
+		records = new ArrayList<LogEntry>();
+	}
+	
+	public void readFile(String fileName) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\keith\\git\\repository\\Module3-Java-Programming-Arrays-Lists-data\\src\\data\\"+fileName));
+		
+		String line;
+		
+		while((line = br.readLine()) != null)
+		{
+			records.add(WebLogParser.parseEntry(line));
+		}
+	}
+	
+	public void printAll() {
+		
+		for(LogEntry le : records) {
+			
+			System.out.println(le);
+		}
+	}
+	
+
+}
---
+++ Module3-Java-Programming-Arrays-Lists-data/src/logAnalyzer/LogAnalyzerTest.java
@@ -0,0 +1,50 @@
+/**
+ * Class to test the LogAnalyzer Class
+ * 
+ * Developer: Keith Kretz
+ * Date Created: 7/29/2024
+ * Last Updated: 7/29/2024  
+ */
+package logAnalyzer;
+
+import java.io.IOException;
+import java.util.Date;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class LogAnalyzerTest {
+	
+	/**
+     * Sets up the test fixture.
+     * Called before every test case method.
+     */
+    
+    @Before
+    public void setUp() {
+        
+    }
+    
+    /**
+     * Sets up main call to test method.
+     */
+    
+    @Test
+    public void mainTest() throws IOException{
+         LogAnalyzer la = new LogAnalyzer();
+         la.readFile("short-test_log");
+         la.printAll();
+        
+    }
+
+    /**
+     * Tears down the test fixture.
+     *
+     * Called after every test case method.
+     */
+    @After
+    public void tearDown() {
+    }
+
+}
