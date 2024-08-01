ee305e32271ab84a0fc5a22aee363d583fbf7ed2
keith <keith@DESKTOP-A68GSGO>
2024-08-01 17:39:10 -0400

    Additions: - added the pre-made classes CaesarCipher.java, CaesarCracker.java, VigenereBreacker.java, and VigenereCipher.java -added Test classes fore the CaesarCipher and VigenereCipher class
    Modifications:
    -none
    
    Main
    -Create test classes for CaesarCipher class and VigenereCipher class

---
+++ Module4-Java-Programming-Arrays-Lists-data/src/vigenereCipher/CaesarCipherTest.java
@@ -0,0 +1,57 @@
+package vigenereCipher;
+
+/**
+ * Class to test the CaesarCipher Class
+ * 
+ * Developer: Keith Kretz
+ * Date Created: 8/1/2024
+ * Last Updated: 8/1/2024  
+ */
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CaesarCipherTest {
+	
+	/**
+     * Sets up the test fixture.
+     * Called before every test case method.
+     */
+    
+    @Before
+    public void setUp() {
+        
+    }
+    
+    /**
+     * Sets up main call to test method.
+     */
+    
+    @Test
+    public void mainTest1() throws IOException{
+        
+        
+    }
+    
+    @Test
+    public void mainTest2() throws IOException {
+    	
+    	
+    }
+
+    /**
+     * Tears down the test fixture.
+     *
+     * Called after every test case method.
+     */
+    @After
+    public void tearDown() {
+    }
+
+}
