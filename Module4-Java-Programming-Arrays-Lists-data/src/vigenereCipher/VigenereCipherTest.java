0ab55e4c127620aca3e2ff5bbd31ba2a4f076dc5
keith <keith@DESKTOP-A68GSGO>
2024-08-05 15:12:37 -0400

    Additions: -none
    Modifications:
    -added a test to VigenereCiperTest.java
    
    Main:
    -Create a test for VigenereCipher.java for the constructor and method
    encrpyt() which results are as expected

--- Module4-Java-Programming-Arrays-Lists-data/src/vigenereCipher/VigenereCipherTest.java
+++ Module4-Java-Programming-Arrays-Lists-data/src/vigenereCipher/VigenereCipherTest.java
@@ -1,5 +1,8 @@
 package vigenereCipher;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 /**
  * Class to test the VigenereCipher Class
  * 
@@ -35,8 +38,26 @@ public class VigenereCipherTest {
     
     @Test
     public void mainTest1() throws IOException{
-        
-        
+    	BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\keith\\git\\repository\\Module4-Java-Programming-Arrays-Lists-data\\src\\data\\titus-small.txt"));
+
+    	String lines;
+    	ArrayList<String> story = new ArrayList<String>();
+    	
+    	while((lines =br.readLine()) != null)
+    	{
+    		story.add(lines);
+    	}
+    	
+    	String trueStory ="";
+    	for(String line : story)
+    	{
+    		trueStory = trueStory+line+" ";
+    	}
+        int[] key = new int[] {17,14,12,4};
+    	
+    	VigenereCipher vg = new VigenereCipher(key);
+    	
+    	System.out.println(vg.encrypt(trueStory));
     }
     
     @Test
