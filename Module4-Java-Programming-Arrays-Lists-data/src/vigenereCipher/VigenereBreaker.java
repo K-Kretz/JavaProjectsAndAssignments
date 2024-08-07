1fb37e0e5cad07206e43fd572bb598eb582c367f
keith <keith@DESKTOP-A68GSGO>
2024-08-07 16:29:07 -0400

    Additions: -Added Language Dictionaries
    Modifications:
    -Updated tests for the exam questions
    
    Main:
    -Tests were updated to match exam questions on VigenereCipherTest.java

--- Module4-Java-Programming-Arrays-Lists-data/src/vigenereCipher/VigenereBreaker.java
+++ Module4-Java-Programming-Arrays-Lists-data/src/vigenereCipher/VigenereBreaker.java
@@ -1,9 +1,13 @@
 package vigenereCipher;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
+
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
 
 public class VigenereBreaker {
 	
@@ -32,11 +36,152 @@ public class VigenereBreaker {
         
         return key;
     }
+    
+    public HashSet<String> readDictionary(File f) throws IOException{
+		
+    	HashSet<String> hs = new HashSet<String>();
+    	
+    	BufferedReader br = new BufferedReader(new FileReader(f));
 
+    	String lines;
+    	
+    	while((lines =br.readLine()) != null)
+    	{
+    		hs.add(lines);
+    	}
+    	
+    	//System.out.println("Size of dictionary: "+hs.size());
+    	
+    	return hs;
+    }
+
+    public int countWords(String message, HashSet<String> dictionary) {
+    	
+    	int countRealWords = 0;
+    	
+    	for(String word : message.toLowerCase().split("\\W"))
+    	{
+    		if(dictionary.contains(word))
+    		{
+    			//System.out.println(word);
+    			countRealWords +=1;
+    		}
+    	}
+    	
+    	return countRealWords;
+    }
+    
+    public String breakForLanguage(String encrypted, HashSet<String> dictionary) {
+    	
+    	String message = "";
+    	int maxCount = 0;
+    	int keyLengthcount=0;
+    	
+    	for(int i=1; i<=100; i++)
+    	{
+    		if(encrypted.length()>=100)
+    		{
+    			char letter = mostCommonCharIn(dictionary);
+    			int[] key = tryKeyLength(encrypted, i, letter);
+    			VigenereCipher vc = new VigenereCipher(key);
+    			
+    			String decryptMessage = vc.decrypt(encrypted);
+    			int count = countWords(decryptMessage,dictionary);
+    			
+    			if(count > maxCount)
+    			{
+    				maxCount=count;
+    				message = decryptMessage;
+    				keyLengthcount = i;
+    			}
+    		}
+    	}
+    	
+    	System.out.println("Real Words: "+maxCount);
+    	System.out.println("Key Length: "+keyLengthcount);
+    	
+    	return message;
+    }
+    
+    public char mostCommonCharIn(HashSet<String> dictionary) {
+		
+    	char mostCommonLetter = '\0';
+    	int mostCommonLetterValue = 0;
+    	
+    	HashMap<Character, Integer> characterCounter = new HashMap<Character, Integer>();
+    	
+
+    	
+    	for(String word : dictionary)
+    	{
+    		char[] letters = word.toCharArray();
+    		
+    		for(int i=0; i<letters.length; i++)
+    		{
+    			if(!characterCounter.containsKey(letters[i]))
+    			{
+    				characterCounter.put(letters[i], 1);
+    			}
+    			else
+    			{
+    				characterCounter.replace(letters[i],characterCounter.get(letters[i])+1);
+    			}
+    		}
+    	}
+    	for(Character character : characterCounter.keySet())
+    	{
+    		if(characterCounter.get(character)>mostCommonLetterValue)
+    		{
+    			mostCommonLetter = character;
+    			mostCommonLetterValue = characterCounter.get(character);
+    		}
+    	}
+	
+    	return mostCommonLetter;	
+    }
+    
+    public String breakForAllLangs(String encrypted, HashMap<String,HashSet<String>> languages) {
+		
+    	String message="";
+    	String codedLanguage="";
+    	int maxCount = 0;
+    	
+    	for(String language : languages.keySet())
+    	{
+    		String breakWithThisLanguageMessage = breakForLanguage(encrypted, languages.get(language));
+    		int count = countWords(breakWithThisLanguageMessage, languages.get(language));
+    		if(count>maxCount)
+    		{
+    			message = breakWithThisLanguageMessage;
+    			maxCount = count;
+    			codedLanguage = language;
+    		}
+    	} 	
+    	
+    	System.out.println("Was coded in language: "+codedLanguage);
+    	
+    	return message;
+    }
+    public File[] DirectoryResource(final JFrame frame) throws IOException{  
+    	
+    	File[] files = null;
+    	
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setMultiSelectionEnabled(true);
+
+        int option = fileChooser.showOpenDialog(frame);
+        if(option == JFileChooser.APPROVE_OPTION)
+        {
+           files = fileChooser.getSelectedFiles();
+        } 
+        
+        return files;
+	}
+    
     public void breakVigenere () throws IOException {
        
 
-    	BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\keith\\git\\repository\\Module4-Java-Programming-Arrays-Lists-data\\src\\data\\athens_keyflute.txt"));
+    	BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\keith\\git\\repository\\Module4-Java-Programming-Arrays-Lists-data\\src\\data\\secretmessage2.txt"));
 
     	String lines;
     	ArrayList<String> story = new ArrayList<String>();
@@ -52,15 +197,25 @@ public class VigenereBreaker {
     		trueStory = trueStory+line+" ";
     	}
     	
-    	int[] key = tryKeyLength(trueStory, 5, 'e');
     	
-    	VigenereCipher vc = new VigenereCipher(key);
     	
-    	String decryptedStory = vc.decrypt(trueStory);
     	
-    	System.out.println(decryptedStory);
+    	File f = new File("C:\\Users\\keith\\git\\repository\\Module4-Java-Programming-Arrays-Lists-data\\src\\data\\English");
+    	HashSet<String> dictionary = readDictionary(f);
+    	
+    	
+    	
+    	
+    
+    	
+    	
+    	String decryptedStory = breakForLanguage(trueStory, dictionary);
+    	
+    	//System.out.println(decryptedStory);
     	
     	
     }
+    
+    
     
 }
--- Module4-Java-Programming-Arrays-Lists-data/src/vigenereCipher/VigenereCipherTest.java
+++ Module4-Java-Programming-Arrays-Lists-data/src/vigenereCipher/VigenereCipherTest.java
@@ -1,6 +1,7 @@
 package vigenereCipher;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 
 /**
@@ -15,8 +16,12 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.HashSet;
+
+import javax.swing.JFrame;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -36,16 +41,174 @@ public class VigenereCipherTest {
      * Sets up main call to test method.
      */
     
+//    @Test
+//    public void mainTest1() throws IOException{
+//    	BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\keith\\git\\repository\\Module4-Java-Programming-Arrays-Lists-data\\src\\data\\titus-small.txt"));
+//
+//    	String lines;
+//    	ArrayList<String> story = new ArrayList<String>();
+//    	
+//    	while((lines =br.readLine()) != null)
+//    	{
+//    		story.add(lines);
+//    	}
+//    	
+//    	String trueStory ="";
+//    	for(String line : story)
+//    	{
+//    		trueStory = trueStory+line+" ";
+//    	}
+//        int[] key = new int[] {17,14,12,4};
+//    	
+//    	VigenereCipher vg = new VigenereCipher(key);
+//    	
+//    	System.out.println(vg.encrypt(trueStory));
+//    }
+//    
+//    @Test
+//    public void mainTest2() throws IOException {
+//    	
+//    	BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\keith\\git\\repository\\Module4-Java-Programming-Arrays-Lists-data\\src\\data\\titus-small.txt"));
+//
+//    	String lines;
+//    	ArrayList<String> story = new ArrayList<String>();
+//    	
+//    	while((lines =br.readLine()) != null)
+//    	{
+//    		story.add(lines);
+//    	}
+//    	
+//    	String trueStory ="";
+//    	for(String line : story)
+//    	{
+//    		trueStory = trueStory+line+" ";
+//    	}
+//    	
+//    	VigenereBreaker vb = new VigenereBreaker();
+//    	
+//    	System.out.println(vb.sliceString("abcdefghijklm", 0, 3));
+//    	
+//    	System.out.println(vb.sliceString("abcdefghijklm", 1, 3));
+//    	
+//    	System.out.println(vb.sliceString("abcdefghijklm", 4, 5));
+//    	
+//    }
+//    
+//    @Test
+//    public void mainTest3() throws IOException {
+//    	
+//    	BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\keith\\git\\repository\\Module4-Java-Programming-Arrays-Lists-data\\src\\data\\secretmessage1.txt"));
+//
+//    	String lines;
+//    	ArrayList<String> story = new ArrayList<String>();
+//    	
+//    	while((lines =br.readLine()) != null)
+//    	{
+//    		story.add(lines);
+//    	}
+//    	
+//    	String trueStory ="";
+//    	for(String line : story)
+//    	{
+//    		trueStory = trueStory+line+" ";
+//    	}
+//    	
+//    	VigenereBreaker vb = new VigenereBreaker();
+//    	
+//    	int[] key = vb.tryKeyLength(trueStory, 4, 'e');
+//    	
+//    	for(int num : key)
+//    	{
+//    		System.out.println(num);
+//    	}
+//    }
+//    
+//    @Test
+//    public void mainTest4() throws IOException {
+//    	
+//    	
+//    	VigenereBreaker vb = new VigenereBreaker();
+//    	
+//    	vb.breakVigenere();
+//    	
+//    	
+//    }
+    
+//    @Test
+//    public void mainTest5() throws IOException {    	
+//    	// Quiz 1
+//    			VigenereBreaker v = new VigenereBreaker();
+//    			BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\keith\\git\\repository\\Module4-Java-Programming-Arrays-Lists-data\\src\\data\\secretmessage2.txt"));
+//    			String lines1;
+//    	    	ArrayList<String> story = new ArrayList<String>();
+//    	    	
+//    	    	while((lines1 =br.readLine()) != null)
+//    	    	{
+//    	    		story.add(lines1);
+//    	    	}
+//    	    	
+//    	    	String trueStory ="";
+//    	    	for(String line : story)
+//    	    	{
+//    	    		trueStory = trueStory+line+" ";
+//    	    	}
+//    	    	
+//    			File f = new File("C:\\Users\\keith\\git\\repository\\Module4-Java-Programming-Arrays-Lists-data\\src\\data\\English");
+//    			HashSet<String> dictionary = v.readDictionary(f);
+//    			String decrypt = v.breakForLanguage(trueStory, dictionary);
+//    			System.out.println(decrypt);
+////    			// Quiz 2
+//    			int count = v.countWords(decrypt, dictionary);
+//   			    System.out.println(count + " valid words are in the decrypted String.");
+////    			
+////    			// Quiz 3
+////    			String [] lines = decrypt.split("\\r?\\n");
+////    			System.out.println("The first line of text is " + lines[0]);
+//    			
+//    			// Quiz 4
+//    			int [] key = v.tryKeyLength(trueStory, 38, 'e');
+//    			VigenereCipher vc = new VigenereCipher(key);
+//    	    	decrypt = vc.decrypt(trueStory);
+//    	    	count = v.countWords(decrypt, dictionary);
+//    	    	System.out.println(count + " valid words are in the decrypted String.");
+//    }
+//    @Test
+//    public void mainTest6() throws IOException {    	
+//    	
+//    	VigenereBreaker vb = new VigenereBreaker();
+//    	File f = new File("C:\\Users\\keith\\git\\repository\\Module4-Java-Programming-Arrays-Lists-data\\src\\data\\Italian");
+//    	HashSet<String> dictionary = vb.readDictionary(f);
+//    	char mostCommonLetter = vb.mostCommonCharIn(dictionary);
+//    	System.out.println(mostCommonLetter);
+//    	
+//    	Assert.assertEquals(mostCommonLetter, 'a');
+//    }
     @Test
-    public void mainTest1() throws IOException{
-    	BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\keith\\git\\repository\\Module4-Java-Programming-Arrays-Lists-data\\src\\data\\titus-small.txt"));
+    public void mainTest7() throws IOException {    	
+    	
+    	VigenereBreaker vb = new VigenereBreaker();
+    	
+    	JFrame frame = new JFrame();
 
-    	String lines;
+		File[] files = vb.DirectoryResource(frame);	
+    	
+		HashMap<String,HashSet<String>> languages = new HashMap<String, HashSet<String>>();
+		
+    	for(File file : files)
+    	{
+    		String lang = file.getAbsolutePath().substring(file.getAbsolutePath().lastIndexOf("\\")+1);
+    		languages.put(lang, vb.readDictionary(file));
+    	}
+//    	System.out.println(languages.get("English").size());
+//    	System.out.println(languages.get("Dutch").size());
+    	
+    	BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\keith\\git\\repository\\Module4-Java-Programming-Arrays-Lists-data\\src\\data\\secretmessage4.txt"));
+		String lines1;
     	ArrayList<String> story = new ArrayList<String>();
     	
-    	while((lines =br.readLine()) != null)
+    	while((lines1 =br.readLine()) != null)
     	{
-    		story.add(lines);
+    		story.add(lines1);
     	}
     	
     	String trueStory ="";
@@ -53,80 +216,11 @@ public class VigenereCipherTest {
     	{
     		trueStory = trueStory+line+" ";
     	}
-        int[] key = new int[] {17,14,12,4};
     	
-    	VigenereCipher vg = new VigenereCipher(key);
-    	
-    	System.out.println(vg.encrypt(trueStory));
-    }
-    
-    @Test
-    public void mainTest2() throws IOException {
-    	
-    	BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\keith\\git\\repository\\Module4-Java-Programming-Arrays-Lists-data\\src\\data\\titus-small.txt"));
-
-    	String lines;
-    	ArrayList<String> story = new ArrayList<String>();
-    	
-    	while((lines =br.readLine()) != null)
-    	{
-    		story.add(lines);
-    	}
-    	
-    	String trueStory ="";
-    	for(String line : story)
-    	{
-    		trueStory = trueStory+line+" ";
-    	}
-    	
-    	VigenereBreaker vb = new VigenereBreaker();
-    	
-    	System.out.println(vb.sliceString("abcdefghijklm", 0, 3));
-    	
-    	System.out.println(vb.sliceString("abcdefghijklm", 1, 3));
-    	
-    	System.out.println(vb.sliceString("abcdefghijklm", 4, 5));
-    	
-    }
-    
-    @Test
-    public void mainTest3() throws IOException {
-    	
-    	BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\keith\\git\\repository\\Module4-Java-Programming-Arrays-Lists-data\\src\\data\\athens_keyflute.txt"));
-
-    	String lines;
-    	ArrayList<String> story = new ArrayList<String>();
-    	
-    	while((lines =br.readLine()) != null)
-    	{
-    		story.add(lines);
-    	}
-    	
-    	String trueStory ="";
-    	for(String line : story)
-    	{
-    		trueStory = trueStory+line+" ";
-    	}
-    	
-    	VigenereBreaker vb = new VigenereBreaker();
-    	
-    	int[] key = vb.tryKeyLength(trueStory, 5, 'e');
-    	
-    	for(int num : key)
-    	{
-    		System.out.println(num);
-    	}
-    }
-    
-    @Test
-    public void mainTest4() throws IOException {
-    	
-    	
-    	VigenereBreaker vb = new VigenereBreaker();
-    	
-    	vb.breakVigenere();
-    	
-    	
+     	String messageDecrypted = vb.breakForAllLangs(trueStory,languages);
+    	System.out.println(messageDecrypted);
+//    	
+//    	Assert.assertEquals(mostCommonLetter, 'a');
     }
 
     /**
