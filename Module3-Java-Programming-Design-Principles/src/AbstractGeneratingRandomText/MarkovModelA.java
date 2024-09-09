24b488f8ad3f76c07ee1baa598fd2102eb1ba43f
K-Kretz <k.kretz@aol.com>
2024-09-09 16:56:18 -0400

    Updated MarkovModelA to handle the correct data from examples

--- Module3-Java-Programming-Design-Principles/src/AbstractGeneratingRandomText/MarkovModelA.java
+++ Module3-Java-Programming-Design-Principles/src/AbstractGeneratingRandomText/MarkovModelA.java
@@ -1,17 +1,41 @@
 package AbstractGeneratingRandomText;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class MarkovModelA extends AbstractMarkovModel{
 
-	private int myOrder;
+	private int model;
 	
-	public MarkovModelA(int order) {
-		myOrder = order;
-	}
+	 public MarkovModelA(int n) 
+	 {
+	        model = n;
+	        myRandom = new Random();
+	 }
 	
-	@Override
-	public String getRandomText(int numChars) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	public String getRandomText(int numChars)
+	{ 
+        if(myText==null)
+           return "";
+       
+       StringBuilder sb = new StringBuilder();
+       int index = myRandom.nextInt(myText.length()-model);
+       String key = myText.substring(index,index+model);
+       sb.append(key);
+   
+       for(int k=0; k < numChars-model; k++)
+       {
+           ArrayList<String> follows = getFollows(key);
+           //System.out.println("key " + key + " " + follows);
+           if(follows.size() == 0)
+               break;
+       
+           index = myRandom.nextInt(follows.size());
+           String next = follows.get(index);
+           sb.append(next);
+           key = key.substring(1) + next;
+       }   
+       return sb.toString();
+   }
 
 }
---
+++ Module3-Java-Programming-Design-Principles/src/AbstractGeneratingRandomText/Tester.java
@@ -0,0 +1,29 @@
+package AbstractGeneratingRandomText;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.Test;
+
+import AbstractGeneratingRandomText.MarkovRunnerWithInterface;
+
+class Tester {
+
+//	@Test
+//	void testRunMarkovZero() throws IOException {
+//		MarkovRunner mr = new MarkovRunner();
+//		mr.runMarkovZero();
+//	}
+//	@Test
+//	void testRunMarkovZero() throws IOException {
+//		MarkovRunner mr = new MarkovRunner();
+//		mr.runMarkovZero();
+//	}
+	@Test
+	void testRunMarkovZero() throws IOException {
+		MarkovRunnerWithInterface mrwi = new MarkovRunnerWithInterface();
+		mrwi.runMarkov();
+	}
+
+}
