9b6a3f574e2a8c4d5073611e34c9d398b427dd40
K-Kretz <k.kretz@aol.com>
2024-09-16 17:31:28 -0400

    wf

--- Module-4-Java-Programming-Design-Principles/src/UsingJavaBeyondBlueJ/HelloWorld.java
+++ Module-4-Java-Programming-Design-Principles/src/UsingJavaBeyondBlueJ/HelloWorld.java
@@ -24,21 +24,21 @@ public class HelloWorld {
 //
 //	}
 	
-	//example 3: check if person that runs program provides the right number of arguments
-	public static void main(String[] args) {
-		
-		if(args.length ==0)
-		{
-			System.out.println("Please specify the filename");
-			System.exit(1);
-		}
-		FileResource hello = new FileResource(args[0]);
-		for(String line : hello.lines())
-		{
-			if(args.length == 0)
-				System.out.println(line);
-		}
-
-	}
+//	//example 3: check if person that runs program provides the right number of arguments
+//	public static void main(String[] args) {
+//		
+//		if(args.length ==0)
+//		{
+//			System.out.println("Please specify the filename");
+//			System.exit(1);
+//		}
+//		FileResource hello = new FileResource(args[0]);
+//		for(String line : hello.lines())
+//		{
+//			if(args.length == 0)
+//				System.out.println(line);
+//		}
+//
+//	}
 
 }
